ae06af9422a2201459194c3c154d9149a796a8b2
poymanov <n.poymanov@gmail.com>
2019-01-20 13:45:00 +0300

    Add CORS handler extension

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.21 on 2019-01-17 19:20:15.
+ * Generated for Laravel 5.7.21 on 2019-01-20 13:41:15.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
